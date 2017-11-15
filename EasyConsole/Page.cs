bc34b25bfef7aee53b479fed6fdcf2d8215de44d
Sébastien F <s.finot@casusbelli.fr>
2017-11-15 18:17:45 +0100

    Added the ability to set the Title afterward as there was no reason for it not to change Updated the display of the separator

--- EasyConsole/Page.cs
+++ EasyConsole/Page.cs
@@ -5,7 +5,7 @@ namespace EasyConsole
 {
     public abstract class Page
     {
-        public string Title { get; private set; }
+        public string Title { get; set; }
 
         public Program Program { get; set; }
 
@@ -17,19 +17,22 @@ namespace EasyConsole
 
         public virtual void Display()
         {
+            var length = 3;
             if (Program.History.Count > 1 && Program.BreadcrumbHeader)
             {
                 string breadcrumb = null;
                 foreach (var title in Program.History.Select((page) => page.Title).Reverse())
                     breadcrumb += title + " > ";
                 breadcrumb = breadcrumb.Remove(breadcrumb.Length - 3);
+                length = breadcrumb.Length;
                 Console.WriteLine(breadcrumb);
             }
             else
             {
+                length = Title.Length;
                 Console.WriteLine(Title);
             }
-            Console.WriteLine("---");
+            Console.WriteLine(new string('-', length));
         }
     }
 }
